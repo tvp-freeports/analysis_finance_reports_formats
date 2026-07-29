c40711a17e244ec629447561f4e7647002d593d6
Oreste Sciacqualegni <oreste@archLaptopP1Gen6.localdomain>
2026-07-29 12:17:28 +0200

    first mock version of format repository

--- alghoritms/semistructured/deserialize/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Deserializing algorithms for semi-structured document processing.
-
-This module provides deserializing functions specifically designed for
-semi-structured documents.
-"""
--- alghoritms/semistructured/text_filter/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Text extraction algorithms for semi-structured document processing.
-
-This module provides text extraction functions specifically designed for
-semi-structured documents.
-"""
